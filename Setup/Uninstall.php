a130470babc8c9f32795ea2da173bba452fa9d73
lei <lei@kangaroorewards.com>
2019-01-28 15:24:58 -0500

    remove integration record from db

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -6,7 +6,8 @@ namespace Kangaroorewards\Core\Setup;
 use Magento\Framework\Setup\UninstallInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
-use Kangaroorewards\Core\Helper\KangarooRewardsRequest;;
+use Kangaroorewards\Core\Helper\KangarooRewardsRequest;
+use Magento\Integration\Block\Adminhtml\Integration\Edit\Tab\Info;
 
 class Uninstall implements UninstallInterface
 {
@@ -21,9 +22,6 @@ class Uninstall implements UninstallInterface
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $storeManager = $objectManager
             ->get('\Magento\Store\Model\StoreManagerInterface');
-        $log = $objectManager
-            ->get('\Psr\Log\LoggerInterface');
-        $log->info("[Kangaroo Rewards]-uninstall-step1");
         $integrationFactory = $objectManager
             ->get('\Magento\Integration\Model\IntegrationFactory');
         $oauthService = $objectManager
@@ -38,5 +36,16 @@ class Uninstall implements UninstallInterface
         $data = array("domain" => $baseUrl);
         $sendData = json_encode($data);
         $request->post('magento/unInstall', array("data" => $sendData));
+
+        $integrationService = $objectManager
+            ->get('\Magento\Integration\Api\IntegrationServiceInterface');
+
+        $integrationData = $integrationService->delete($integration->getId());
+        if ($integrationData[Info::DATA_ID]) {
+            //Integration deleted successfully, now safe to delete the associated consumer data
+            if (isset($integrationData[Info::DATA_CONSUMER_ID])) {
+                $oauthService->deleteConsumer($integrationData[Info::DATA_CONSUMER_ID]);
+            }
+        }
     }
 }
