60a2ab6826166cbd832523bddda93cd72437a388
lei <lei@kangaroorewards.com>
2024-11-25 18:01:55 -0500

    Update js file

--- view/frontend/web/js/main.js
+++ view/frontend/web/js/main.js
@@ -5,7 +5,7 @@ define(
     function ($) {
         "use strict";
         return function (config) {
-            KangarooApps = KangarooApps || {};
+            var KangarooApps = KangarooApps || {};
             var productList = [];
             var productDetails = [];
             KangarooApps.Loyalties = KangarooApps.Loyalties || {};
