194f2e356e1ff33c5840c78f4d0d0e111ece8655
moejkhaled <104511743+moejkhaled@users.noreply.github.com>
2025-09-08 19:10:58 +0300

    [NBP-2485] Debug widget

--- Model/KangarooEndpoint.php
+++ Model/KangarooEndpoint.php
@@ -119,6 +119,7 @@ class KangarooEndpoint implements KangarooEndpointInterface
      */
     public function translation($url = null)
     {
+        return json_encode(["message" => "test"]);
         $data = [
             'storeId' => $this->kangarooData->getStoreId(),
             'domain' => $this->kangarooData->getBaseStoreUrl(),
