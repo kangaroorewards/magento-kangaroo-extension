fddfba2f9fdacc4b72ea5c79269e90f3787c7927
moejkhaled <104511743+moejkhaled@users.noreply.github.com>
2025-09-08 21:00:01 +0300

    [NBP-2485] Debug

--- Model/KangarooEndpoint.php
+++ Model/KangarooEndpoint.php
@@ -565,6 +565,7 @@ class KangarooEndpoint implements KangarooEndpointInterface
         ];
 
         if ($this->isCustomerLoggedIn()) {
+            return json_encode(["spin_win_id" => $spinWinId, "status" => 'not-logged-in']);
             $customer = $this->_getCustomer();
             $data['customerEmail'] = $customer->getEmail();
             $data['customerId'] = $customer->getId();
